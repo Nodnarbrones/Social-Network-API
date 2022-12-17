9ccf0079227248f3b53b6aacd820337a1531545a
Nodnarbrones <sr.bvillasenor@gmail.com>
2022-12-16 20:02:21 -0800

    uncommented get timestamp in thought model schema and restructured thought controler

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,97 @@
 const { Thought, User } = require('../models');
+
+const thoughtController = {
+    
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then((dbThoughtData) => {
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });  
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbthoughtData) {
+          return res.status(404).json({ message: 'No Thought has that id' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    },
+
+    createThought(req, res) {
+        Thought.create(req.body)
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    },
+
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought has this id' });
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought has that id' });
+        }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+    },
+
+    createReaction(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $addToSet: { reactions: req.body }}, { runValidators: true, new: true })
+            .then((dbThoughtData) => {
+              if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No thought has that id' });
+              }
+              res.json(dbThoughtData);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+    },
+
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId },{ $pull: { reactions: { reactionId: req.params.reactionId}}},{ runValidators: true, new: true })
+            .then((dbThoughtData) => {
+              if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No thought has that id!' });
+              }
+              res.json(dbThoughtData);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+    },
+};
+
+module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -42,7 +42,7 @@ const thoughtSchema = new Schema(
       createdAt: {
         type: Date,
         default: Date.now,
-        //get: timestamp => dateFormat(timestamp)
+        get: timestamp => dateFormat(timestamp)
       },
       username: {
         type: String,
